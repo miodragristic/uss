70d07a2d44ae42dd62d44b2a1a72c33aff3ac4d1
Miodrag Mio Ristic <mikkyristic@gmail.com>
2025-06-02 01:13:43 +0300

    Opis tvojih izmena

---
+++ app/head.js
@@ -0,0 +1,9 @@
+// app/head.js
+export default function Head() {
+    return (
+      <>
+        <meta name="google-site-verification" content="SEUqHyOo1iZdcIUxaWDgdv7n2dd88hWMaaQipMpoDEM" />
+      </>
+    );
+  }
+  
