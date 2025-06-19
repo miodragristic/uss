fcb4b2dc3952e260f73487aab7c2dcafb7c499b8
Miodrag Mio Ristic <mikkyristic@gmail.com>
2025-06-20 01:59:07 +0300

    Opis promene

--- app/blog/inter-miami-porto/page.jsx
+++ app/blog/inter-miami-porto/page.jsx
@@ -184,6 +184,18 @@ Copyright: 2025 Getty Image</span>
           <br />
           This is no longer a marketing project. This is Inter Miami, rising contenders.
         </p>
+
+        <div className="flex justify-center mb-8">
+  <iframe
+    width="100%"
+    height="400"
+    src="https://www.youtube.com/embed/3WpiStPR820"
+    title="Inter Miami vs Porto Highlights"
+    frameBorder="0"
+    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen
+  ></iframe>
+</div>
       </div>
     </div>
   );
